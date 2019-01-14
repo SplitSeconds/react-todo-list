fd756f6d2a5b9f0927fdbe60a513d7737502a739
Nele Luepkes <nluepkes@gmail.com>
2019-01-14 13:56:13 +0100

    finished react to do without hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import TodoForm from './ToDoForm'
+import TodoList from './ToDoForm'
+import ToDoItems from './ToDoItems'
 import TodoFormHooks from './ToDoForm_Hooks'
 
 class App extends Component {
@@ -12,23 +13,44 @@ class App extends Component {
     }
   }
   handleInput = e => {
-    console.log('Hello Input')
+    const itemText = e.target.value
+    const currentItem = { text: itemText, key: Date.now() }
+    this.setState({
+      currentItem,
+    })
   }
   addItem = e => {
     e.preventDefault()
-    console.log('Hello World')
+    const newItem = this.state.currentItem
+    if (newItem.text !== '') {
+      console.log(newItem)
+      const items = [...this.state.items, newItem]
+      this.setState({
+        items: items,
+        currentItem: { text: '', key: '' },
+      })
+    }
+  }
+  deleteItem = key => {
+    const filteredItems = this.state.items.filter(item => {
+      return item.key !== key
+    })
+    this.setState({
+      items: filteredItems,
+    })
   }
   render() {
     return (
 
       <div className="App">
         <h2>To do</h2>
-        <TodoForm 
+        <TodoList 
           addItem={this.addItem}
           inputElement={this.inputElement}
           handleInput={this.handleInput}
           currentItem={this.state.currentItem}
         />
+        <ToDoItems entries={this.state.items} deleteItem={this.deleteItem} />
         <hr></hr>
         <h2>ToDo with hooks</h2>
         <TodoFormHooks />
